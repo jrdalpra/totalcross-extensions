90d3dc05ddca2b37f544a14467d481a68e273365
José Volmei Dal Prá Junior <jrdalpra@gmail.com>
2013-05-17 21:46:13 -0300

    renames

--- com.wolkenapps.totalcross.utils/src/main/java/com/wolkenapps/totalcross/listeners/KeyListenerAdapter.java
+++ com.wolkenapps.totalcross.utils/src/main/java/com/wolkenapps/totalcross/listeners/KeyListenerAdapter.java
@@ -3,7 +3,7 @@ package com.wolkenapps.totalcross.listeners;
 import totalcross.ui.event.KeyEvent;
 import totalcross.ui.event.KeyListener;
 
-public class AbstractKeyListener implements KeyListener {
+public class KeyListenerAdapter implements KeyListener {
 
    public void actionkeyPressed(KeyEvent event) {
    }
--- com.wolkenapps.totalcross.utils/src/main/java/com/wolkenapps/totalcross/utils/ui/Componentes.java
+++ com.wolkenapps.totalcross.utils/src/main/java/com/wolkenapps/totalcross/utils/ui/Componentes.java
@@ -3,8 +3,26 @@ package com.wolkenapps.totalcross.utils.ui;
 import totalcross.ui.Button;
 import totalcross.ui.Control;
 import totalcross.ui.ToolTip;
+import totalcross.ui.dialog.MessageBox;
 
 public class Componentes {
+
+   public static class Mensagem {
+
+      /**
+       * Retora a resposta a pergunta
+       *
+       * @param titulo
+       * @param mensagem
+       * @return true quando reponser sim e false quando response nao
+       */
+      public static boolean fazPergunta(String titulo, String mensagem) {
+         MessageBox tela = new MessageBox(titulo, mensagem, new String[] { "Sim", "Não" });
+         tela.popup();
+         return tela.getPressedButtonIndex() == 0;
+      }
+
+   }
 
    public static int retornaAMaiorAlturaEntreOsControles(Control[] controles) {
       int maior = 0;
